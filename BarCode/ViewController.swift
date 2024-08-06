58048a4864bf2ab12b74e7e628220a3d6f9019ea
Chenghav <lychenghavsloy@gmail.com>
2024-08-06 15:36:09 +0700

    Initial Commit

---
+++ BarCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarCode
+//
+//  Created by Chenghav on 6/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
