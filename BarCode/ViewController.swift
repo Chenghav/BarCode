1a3361f3a5a91074596bfeda513ae46d550f113f
Chenghav <lychenghavsloy@gmail.com>
2024-08-15 09:00:42 +0700

    first commit

--- BarCode/ViewController.swift
+++ BarCode/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barCodeImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        barCodeImage.image = generateQRCode(from: "00020101021229600009khqr@ppcb011311200012730880226Phnom Penh Commercial Bank520459995303840540513.105802KH5911LY CHENGHAO6010PHNOM PENH9917001317108186256066304D5F6")
     }
 
+//    func generateBarcode(from string: String) -> UIImage? {
+//        let data = string.data(using: String.Encoding.ascii)
+//
+//        if let filter = CIFilter(name: "CICode128BarcodeGenerator") {
+//            filter.setValue(data, forKey: "inputMessage")
+//            let transform = CGAffineTransform(scaleX: 3, y: 3)
+//
+//            if let output = filter.outputImage?.transformed(by: transform) {
+//                return UIImage(ciImage: output)
+//            }
+//        }
+//
+//        return nil
+//    }
+    
+//    func generateQRCode(from string: String) -> UIImage? {
+//        let data = string.data(using: String.Encoding.ascii)
+//
+//        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+//            filter.setValue(data, forKey: "inputMessage")
+//            let transform = CGAffineTransform(scaleX: 3, y: 3)
+//
+//            if let output = filter.outputImage?.transformed(by: transform) {
+//                return UIImage(ciImage: output)
+//            }
+//        }
+//
+//        return nil
+//    }
+    func generateQRCode(from string: String) -> UIImage? {
+           let data = string.data(using: .utf8)
+           guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+           
+           qrFilter.setValue(data, forKey: "inputMessage")
+           guard let qrCodeImage = qrFilter.outputImage else { return nil }
+           
+           let scaleX = UIScreen.main.scale
+           let transformedImage = qrCodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleX))
+           
+           return UIImage(ciImage: transformedImage)
+       }
 
 }
 
